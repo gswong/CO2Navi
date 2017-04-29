4285f5eaeec774fce209f12c3ba09c7518a5aa37
Tudor Fanaru <tf_10@live.com>
2017-04-29 13:07:25 -0700

    updated LineChart

--- chartData.js
+++ chartData.js
@@ -1,9 +1,23 @@
-function giveData() {
-    var data = [];
+function giveCo2Data() {
+    var ret = [];
     $.getJSON("feed.json", function (data) {
 
-        for (var i=0; i<data.length; i++) {
-            data.push(data[i].co2Saved);
+        for (var i=0; i<data.length; i++)
+        {
+            ret.push(data[i].co2Saved);
         }
-    }
+        
+    })
+    return ret;
 }
+function giveDateData() {
+    var ret = [];
+        $.getJSON("feed.json", function (data) {
+            for(var i = 0; i < data.length; i++)
+            {
+                ret.push(getDateTimeString(data[i].date));                
+            }
+    })
+    return ret;
+}
+
--- dateTimeHelper.js
+++ dateTimeHelper.js
@@ -1,10 +1,24 @@
-function getDateString( dateObj ) 
-{
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
-    var hour = dateObj.getHours();
-    var mins = dateObj.getMinutes();
+function getDateTimeString( t ) { //t = MS since 1970
+    
+    var minutes = 1000 * 60;
+    var hours = minutes * 60;
+    var days = hours * 24;
+    var years = days * 365;
+    
 
-    return = year + "/" + month + "/" + day + " " + hour + ":" + "mins";
+
+    var y = Math.floor(t / years);
+    t -= y*years;
+    y += 1970;
+    var day = Math.floor(t / days);
+    t -= day*days;
+    var hour = Math.floor(t / hours);
+    t -= hour * hours;
+    var mins = Math.round(t / minutes);
+
+    // e.x. 2017/183/18/24 = 24th min of 18th hour of 183rd day of 2017
+    
+
+    var res =  y.toString() + "/" + day.toString() + " " + hour.toString() + ":" + mins.toString();
+    return res;
 }
